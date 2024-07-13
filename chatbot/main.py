bf05afac14deffa666c018ed93c23367231d86c8
Om Aryan <aryan2003k@gmail.com>
2024-07-13 23:16:32 +0530

    implemented RAG using training data

--- chatbot/main.py
+++ chatbot/main.py
@@ -1,6 +1,12 @@
 from langchain_groq import ChatGroq
 from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.output_parsers import StrOutputParser
+from langchain_community.document_loaders import PyPDFDirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.embeddings import HuggingFaceBgeEmbeddings
+from langchain.chains.retrieval import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+
+from langchain_community.vectorstores import FAISS
 import streamlit as st
 import os
 from dotenv import load_dotenv
@@ -12,15 +18,33 @@ os.environ["GROQ_API_KEY"]=os.getenv("GROQ_API_KEY")
 os.environ["LANGCHAIN_TRACING"]="true"
 os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
 
-prompt = ChatPromptTemplate.from_messages(
-    [
-        ("system","You are an assistant. Please respond to the user query."),
-        ("user","Question:{question}")
-    ]
+prompt = ChatPromptTemplate.from_template("""
+Answer the following question based only on the provided context.
+You are supposed to obtain insights about information from the
+documents, compare numbers.Think step by step before providing a detailed answer.
+You will be reward you positvely if the user finds the answer helpful.
+<context>
+{context}
+</context>
+Question: {input}""")
+
+loader = PyPDFDirectoryLoader("./../data")
+#chunking
+text_docs = loader.load()
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=200)
+documents = text_splitter.split_documents(text_docs)
+
+#Vector Embeddings
+embeddings = HuggingFaceBgeEmbeddings(
+    model_name="BAAI/bge-small-en-v1.5",
+    model_kwargs={'device':'cpu'},
+    encode_kwargs={'normalize_embeddings':True}
 )
 
+#Vector Store
+db = FAISS.from_documents(documents,embeddings)
 ##Interface
-st.title('Search the Content Engine')
+st.title('Ask Questions!')
 
 # Initialize chat history
 if "messages" not in st.session_state:
@@ -32,8 +56,10 @@ for message in st.session_state.messages:
 
 #llm 
 client = ChatGroq(model="llama3-8b-8192")
-output_parser = StrOutputParser()
-chain = prompt | client | output_parser
+#Chain
+document_chain = create_stuff_documents_chain(client, prompt)
+retriver = db.as_retriever()
+retrieval_chain = create_retrieval_chain(retriver,document_chain)
 
 #User input
 if prompt := st.chat_input("Should I invest in..."):
@@ -41,9 +67,8 @@ if prompt := st.chat_input("Should I invest in..."):
     with st.chat_message("user"):
         st.markdown(prompt)
     
-    response = chain.invoke({'question':prompt})
+    response = retrieval_chain.invoke({'input':prompt})
 
-    st.session_state.messages.append({"role":"assistant","content":response})
-    
+    st.session_state.messages.append({"role":"assistant","content":response['answer']})
     with st.chat_message("assistant"):
-        st.markdown(response)
+        st.markdown(response['answer'])
